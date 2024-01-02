2daf8d1c3b6fcd09193037a465992092687f40fc
isujin <lsj0232191@gmail.com>
2024-01-01 22:08:21 -0500

    word cloud

--- filtered_reviews.py
+++ filtered_reviews.py
@@ -1,4 +1,36 @@
 import json
+from collections import defaultdict
+import string
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+def calculate_word_frequencies(reviews):
+    word_freq_by_restaurant = defaultdict(lambda: defaultdict(int))
+    stop_words = set(stopwords.words('english'))
+
+    for review in reviews:
+        # print(f"Review: {review}")
+        text = review.get('text', '').lower()
+        # print(f"Review Text: {text}")
+        tokens = word_tokenize(text)
+
+        # Remove punctuation and filter out stop words
+        tokens = [token for token in tokens if token.isalpha() and token not in string.punctuation]
+        filtered_tokens = [token for token in tokens if token not in stop_words]
+
+        # Count word frequencies by restaurant
+        word_freq = defaultdict(int)
+        for token in filtered_tokens:
+            word_freq[token] += 1
+
+        # print(f"Word Frequencies: {word_freq}")
+
+        # Update the word frequency dictionary for the respective restaurant
+        business_id = review.get('business_id')
+        word_freq_by_restaurant[business_id].update(word_freq)
+
+    return word_freq_by_restaurant
+
 
 def create_filtered_reviews(filtered_restaurants_file, reviews_file, output_file):
     # Load filtered restaurants from JSON file
@@ -30,11 +62,22 @@ def create_filtered_reviews(filtered_restaurants_file, reviews_file, output_file
         business_id = restaurant.get('business_id')
         reviews_for_restaurant = reviews_by_business.get(business_id, [])
         restaurant['reviews'] = reviews_for_restaurant
-        filtered_reviews.append(restaurant)
+        
+        word_frequencies = calculate_word_frequencies(reviews_for_restaurant)
+
+        # print(f"Business ID: {business_id}, Word Frequencies: {word_frequencies}")
+
+        # Flatten the word frequency dictionary to a list of dictionaries
+        flattened_word_freq = [{'word': word, 'count': count} for word_dict in word_frequencies.values() for word, count in word_dict.items()]
+
+        restaurant['word_frequencies'] = flattened_word_freq
+
+        filtered_reviews.append(restaurant)    
+
 
     # Save the result to filtered_reviews.json
     with open(output_file, 'w', encoding='utf-8') as f:
         json.dump(filtered_reviews, f, indent=2)
 
 # Example usage
-create_filtered_reviews('filtered_restaurants.json', 'data/yelp_dataset/yelp_academic_dataset_review.json', 'filtered_reviews.json')
+create_filtered_reviews('filtered_restaurants.json', 'yelp_academic_dataset_review.json', 'filtered_reviews.json')
